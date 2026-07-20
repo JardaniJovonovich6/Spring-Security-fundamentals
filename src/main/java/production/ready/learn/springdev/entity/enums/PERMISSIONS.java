359ba7f512719edd99588f6869074fb4aa540bbc
Aman_Kumar_shaw_5 <ashaw8176@gmail.com>
2026-07-20 13:35:18 +0530

    PERMISSIONS added for ROLE & PERMISSIONS based authorization

---
+++ src/main/java/production/ready/learn/springdev/entity/enums/PERMISSIONS.java
@@ -0,0 +1,16 @@
+package production.ready.learn.springdev.entity.enums;
+
+public enum PERMISSIONS {
+
+    POST_VIEW,
+    POST_CREATE,
+    POST_UPDATE,
+    POST_DELETE,
+
+    USER_VIEW,
+    USER_CREATE,
+    USER_UPDATE,
+    USER_DELETE;
+
+
+}
